9253a0161a7a1a2a0e2990a6826dec1ffbfc0fc3
William Young <william@werl.me>
2020-04-12 17:52:17 -0500

    Add BlockItem, models, & blockstate to example_ore

--- src/main/java/tech/unstable/mods/example_mod/ModEventSubscriber.java
+++ src/main/java/tech/unstable/mods/example_mod/ModEventSubscriber.java
@@ -2,12 +2,14 @@ package tech.unstable.mods.example_mod;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
+import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.registries.IForgeRegistryEntry;
+import tech.unstable.mods.example_mod.init.ModBlocks;
 import tech.unstable.mods.example_mod.init.ModItemGroups;
 
 @Mod.EventBusSubscriber(modid = ExampleMod.MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
@@ -15,6 +17,7 @@ public class ModEventSubscriber {
     @SubscribeEvent
     public static void onRegisterItems(RegistryEvent.Register<Item> event) {
         event.getRegistry().registerAll(
+            setup(new BlockItem(ModBlocks.EXAMPLE_ORE.get(), new Item.Properties().group(ModItemGroups.MOD_ITEM_GROUP)), ModBlocks.EXAMPLE_ORE.get().getRegistryName())
         );
     }
 
